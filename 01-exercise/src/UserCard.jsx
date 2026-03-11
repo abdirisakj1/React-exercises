6942d6b57b1980b5a6f591c13366406b61e9db68
abdirisakj1 <abdirisakjaran@gmail.com>
2026-03-11 13:24:47 +0300

    submited exercise 1 react js

---
+++ 01-exercise/src/UserCard.jsx
@@ -0,0 +1,12 @@
+const UserCard = () => {
+    const userName = "Ahmed Jaamac";
+    const email = "ahmedjamac2@gmail.com"
+    return(
+        <>
+        <h2>{userName}</h2>
+        <p>{email}</p>
+        </>
+    )
+}
+
+export default UserCard
