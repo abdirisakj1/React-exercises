4ed5aff0f8acf5aff3da78cc722a51aa0eec0dfe
abdirisakj1 <abdirisakjaran@gmail.com>
2026-03-17 15:16:18 +0300

    submited exercise 10

--- 10-exercise/src/UserList.jsx
+++ 10-exercise/src/UserList.jsx
@@ -1,30 +1,30 @@
 const UserList = () => {
   const listUsers = [
-    // {
-    //   id: 1,
-    //   name: "jaamaca",
-    //   email: "jaamac@gmail.com",
-    // },
-    // {
-    //   id: 2,
-    //   name: "ahmed",
-    //   email: "ahmed@gmail.com",
-    // },
-    // {
-    //   id: 3,
-    //   name: "nuur",
-    //   email: "nuur@gmail.com",
-    // },
-    // {
-    //   id: 4,
-    //   name: "hasan",
-    //   email: "hassan@gmail.com",
-    // },
-    // {
-    //   id: 5,
-    //   name: "Abdi",
-    //   email: "abdi@gmail.com",
-    // },
+    {
+      id: 1,
+      name: "jaamaca",
+      email: "jaamac@gmail.com",
+    },
+    {
+      id: 2,
+      name: "ahmed",
+      email: "ahmed@gmail.com",
+    },
+    {
+      id: 3,
+      name: "nuur",
+      email: "nuur@gmail.com",
+    },
+    {
+      id: 4,
+      name: "hasan",
+      email: "hassan@gmail.com",
+    },
+    {
+      id: 5,
+      name: "Abdi",
+      email: "abdi@gmail.com",
+    },
   ];
   return (
     <>
