b85e19ebd514e1bca7c29350ab64fb8d209bb068
abdirisakj1 <abdirisakjaran@gmail.com>
2026-03-17 15:10:07 +0300

    submited exercise 10

---
+++ 10-exercise/src/App.jsx
@@ -0,0 +1,10 @@
+import UserList from "./UserList"
+const App = () => {
+  return(
+    <>
+    <UserList/>
+    </>
+  )
+}
+
+export default App
---
+++ 10-exercise/src/UserList.jsx
@@ -0,0 +1,47 @@
+const UserList = () => {
+  const listUsers = [
+    {
+      id: 1,
+      name: "jaamaca",
+      email: "jaamac@gmail.com",
+    },
+    {
+      id: 2,
+      name: "ahmed",
+      email: "ahmed@gmail.com",
+    },
+    {
+      id: 3,
+      name: "nuur",
+      email: "nuur@gmail.com",
+    },
+    {
+      id: 4,
+      name: "hasan",
+      email: "hassan@gmail.com",
+    },
+    {
+      id: 5,
+      name: "Abdi",
+      email: "abdi@gmail.com",
+    },
+  ];
+  return (
+    <>
+      <h2>List Of Users</h2>
+
+     {
+        listUsers.length === 0 ? <p>Not found users!</p> : 
+         <ul>
+        {listUsers.map((user) => (
+          <li key={user.id}>
+            {user.name}, <span>({user.email})</span>
+          </li>
+        ))}
+      </ul>
+     }
+    </>
+  );
+};
+
+export default UserList;
